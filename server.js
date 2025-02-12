299f62961b8c6d1ee694a5382dfaadea4493f85e
s1rojov <sirojoff2727@gmail.com>
2025-02-12 09:46:13 +0500

    fix: unuseful files removed

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ import dotenv from 'dotenv';
 // Environment variablesni yuklaymiz
 dotenv.config();
 
-import './src/bot/saver-bot.js';
 import './src/bot/main-bot.js';
-import router from './src/routes/index.js';
+// import router from './src/routes/index.js';
 
 const app = express();
 
@@ -15,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API route
-app.use('/api', router);
+// app.use('/api', router);
 
 const PORT = process.env.PORT || 3000;
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,5 @@
 import { Router } from 'express';
-import upload from './crypto.js'
 const router = Router();
-
-router.use('/crypto', upload);
 
 
 export default router;
